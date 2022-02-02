74dcfb949ac6c4542c9df47088db6d324085c3b7
Alex Paul <al.9thousand@gmail.com>
2022-02-02 13:43:40 -0500

    map shows on page

---
+++ src/Components/map/Map.js
@@ -0,0 +1,33 @@
+import GoogleMapReact from 'google-map-react'
+import { API_KEY } from '../../secrets';
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/map-marker'
+
+const LocationPin = ({ text }) => (
+  <div className="pin">
+    <Icon icon={locationIcon} className="pin-icon" />
+    <p className="pin-text">{text}</p>
+  </div>
+)
+
+const Map = ({ location, zoomLevel }) => (
+  <div className="map">
+    {/* <h2 className="map-h2">Find your Location</h2> */}
+
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: API_KEY }}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      >
+        <LocationPin
+          lat={location.lat}
+          lng={location.lng}
+          text={location.address}
+        />
+      </GoogleMapReact>
+    </div>
+  </div>
+)
+
+export default Map
--- src/Pages/FindSpot.js
+++ src/Pages/FindSpot.js
@@ -1,23 +1,34 @@
 import Header from "../Components/Header"
+import Map from "../Components/map/Map"
 
 const FindSpot = () => {
+  const location = {lat: 40.748817, lng: -73.985428, address: "20 W 34th St, New York, NY 10001", name: "Empire State Building"};
+
   return (
     <div>
       <Header headerText = {"Find a Spot"}/>
       <div className="select-panel">
-        <label htmlFor="cars">Choose Car Size:</label>
-          <select name="cars"  className="car-size-select">
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
-          </select>
-        <label htmlFor="distance">Choose Distance:</label>
-          <select name="distance"  className="distance-select">
-            <option value=".1">0.1 mi</option>
-            <option value=".25">0.25 mi</option>
-            <option value=".5">0.5 mi</option>
-            <option value="1">1 mi</option>
-          </select>
+        <div className="map-section">
+          <Map
+            location={location}
+            zoomLevel={14}
+          />
+        </div>
+        <div className="select-section">
+          <label htmlFor="cars">Choose Car Size:</label>
+            <select name="cars"  className="car-size-select">
+              <option value="small">Small</option>
+              <option value="medium">Medium</option>
+              <option value="large">Large</option>
+            </select>
+          <label htmlFor="distance">Choose Distance:</label>
+            <select name="distance"  className="distance-select">
+              <option value=".1">0.1 mi</option>
+              <option value=".25">0.25 mi</option>
+              <option value=".5">0.5 mi</option>
+              <option value="1">1 mi</option>
+            </select>
+          </div>
       </div>
     </div>
   )
