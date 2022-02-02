801d5afc9ef81ca9d8d4b28e3db480b9c9a97855
Alex Paul <al.9thousand@gmail.com>
2022-02-01 23:27:23 -0500

    this works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' 
+
+import Home from './Pages/Home'
+import Header from './Components/Header'
+import FindSpot from './Pages/FindSpot'
+import './App.css'
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Parking Pal
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      {/* <Route path="/" element={<Home/>}/> */}
+      {/* <Route path="/find-spot" element={<FindSpot/>}/> */}
     </div>
+    // <div>
+    // <Header/>
+      
+    // </div>
   );
 }
 
